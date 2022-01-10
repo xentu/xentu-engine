a61cd88ec9276b9a3fe0093e9849c16e4934b615
Steve <jakiro.fire@gmail.com>
2022-01-10 20:48:38 +0000

    Added some script placeholders for building MacOS/Linux installers.

---
+++ installers/make-x86-deb.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+echo "This script builds the XentuSDK install script for Linux/Debian (x86)"
---
+++ installers/make-x86-mac.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+echo "This script builds the XentuSDK install script for MacOS (x86)"
