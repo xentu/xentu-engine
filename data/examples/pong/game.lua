7a40ef6c8c68f9eead7ce43c0ce6df2b229972c4
Steve <jakiro.fire@gmail.com>
2022-01-14 22:36:04 +0000

    Updated pong example with some sounds.

--- data/examples/pong/game.lua
+++ data/examples/pong/game.lua
@@ -1,82 +1,96 @@
 --[[
 	Pong Game Example
 ]]
-bg_color = Color.from_hex('#333333')
-white = Color.from_hex('#ffffff')
-blue = Color.from_hex('#0000ff')
-green = Color.from_hex('#00FF00')
-player1_y = 250
-ball_x = 300
-ball_y = 300
-ball_speed_x = 3
-ball_speed_y = 3
+colors = {
+   bg = Color.from_hex('#333333'),
+   black = Color.from_hex('#000000'),
+   white = Color.from_hex('#ffffff'),
+   blue = Color.from_hex('#0000ff'),
+   green = Color.from_hex('#00FF00')
+}
+sounds = {}
+player = { y = 250 }
+ball = { x = 300, y = 300, speed_x = 3, speed_y = 3 }
 score = 0
 
 
--- the init event
+-- the init event.
 game.on("init", function()
-	print("Pong Game Example")
-
 	-- create a font.
 	texture2 = assets.load_texture("fonts/arial_20.png", TX_RGBA, TX_CLAMP_TO_BORDER)
 	spritemap2 = assets.load_spritemap("fonts/arial_20.json")
 	font = Font(texture2, spritemap2)
-	font.letter_spacing = 1
-	font.line_height = 24
 
-	renderer.set_clear_color(bg_color)
-	renderer.set_text_color(green)
+	-- load sounds.
+	sounds.bounce1 = assets.load_audio("sounds/bounce1.mp3")
+	sounds.bounce2 = assets.load_audio("sounds/bounce2.mp3")
+	sounds.fail = assets.load_audio("sounds/fail.mp3")
+	
+	-- set some rendering variables.
+	renderer.set_clear_color(colors.bg)
 	renderer.set_blend(true)
 end)
 
 
 -- the update event.
 game.on("update", function(dt)
-	-- move the ball
-	ball_x = ball_x + ball_speed_x
-	ball_y = ball_y + ball_speed_y
+	-- move the ball.
+	ball.x = ball.x + ball.speed_x
+	ball.y = ball.y + ball.speed_y
 
-	-- check for collisions
-	if (ball_y <= 0) then ball_speed_y = 3 end
-	if (ball_y >= 580) then ball_speed_y = -3 end
+	-- check for collisions.
+	if (ball.y <= 0) then
+		ball.speed_y = 3
+		audio.play(sounds.bounce1)
+	end
+	if (ball.y >= 580) then
+		ball.speed_y = -3
+		audio.play(sounds.bounce1)
+	end
 
-	-- check paddle collision
-	if (ball_x <= 30 and ball_y > player1_y - 10 and ball_y < player1_y + 90) then 
-		ball_speed_x = 3
+	-- check paddle collision.
+	if (ball.x <= 30 and ball.y > player.y - 10 and ball.y < player.y + 90) then 
+		ball.speed_x = 3
 		score = score + 1
+		audio.play(sounds.bounce2)
 	end
 	
-	if (ball_x >= 750 and ball_y > player1_y - 10 and ball_y < player1_y + 90) then
-		ball_speed_x = -3
+	if (ball.x >= 750 and ball.y > player.y - 10 and ball.y < player.y + 90) then
+		ball.speed_x = -3
 		score = score + 1
+		audio.play(sounds.bounce2)
 	end
 
-	-- check for endgame condition
-	if (ball_x <= 0 or ball_x >= 780) then 
-		ball_speed_x = 3
-		ball_speed_y = 3
-		ball_x = 300
-		ball_y = 300
+	-- check for endgame condition.
+	if (ball.x <= 0 or ball.x >= 780) then 
+		ball.speed_x = 3
+		ball.speed_y = 3
+		ball.x = 300
+		ball.y = 300
 		score = 0
+		audio.play(sounds.fail)
 	end
 
-	-- handle user input
-	if keyboard.key_down(KB_UP) and player1_y > 8 then player1_y = player1_y - 8 end
-	if keyboard.key_down(KB_DOWN) and player1_y < 492 then player1_y = player1_y + 8 end
+	-- handle user input.
+	if keyboard.key_down(KB_UP) and player.y > 8 then player.y = player.y - 8 end
+	if keyboard.key_down(KB_DOWN) and player.y < 492 then player.y = player.y + 8 end
 end)
 
 
 -- the draw event
 game.on("draw", function()
 	renderer.begin()
-	-- draw line down the middle
-	renderer.draw_rect(white, 399, 0, 2, 600)
-	-- draw the ball
-	renderer.draw_rect(blue, ball_x, ball_y, 20, 20)
-	-- draw the two paddles
-	renderer.draw_rect(white, 10, player1_y, 20, 100)
-	renderer.draw_rect(white, 770, player1_y, 20, 100)
+	-- draw line down the middle.
+	renderer.draw_rect(colors.white, 399, 0, 2, 600)
+	-- draw the ball.
+	renderer.draw_rect(colors.blue, ball.x, ball.y, 20, 20)
+	-- draw the two paddles.
+	renderer.draw_rect(colors.white, 10, player.y, 20, 100)
+	renderer.draw_rect(colors.white, 770, player.y, 20, 100)
 	-- draw the score.
+	renderer.set_text_color(colors.black)
+	renderer.draw_text(font, "Score: " .. score, 361, 21, 100)
+	renderer.set_text_color(colors.green)
 	renderer.draw_text(font, "Score: " .. score, 360, 20, 100)
 	-- present the image.
 	renderer.present()
