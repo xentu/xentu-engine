f97177a748d50f4c6c50da074e404a80abd81fb2
kodaloid <jakiro.fire@gmail.com>
2022-07-25 20:51:01 +0100

    Removed old sdl test code.

--- main.cpp
+++ main.cpp
@@ -18,45 +18,11 @@ using MACHINE_PTR = const std::unique_ptr<xen::XentuMachine>;
 using namespace xen;
 
 
-int sdl_test()
-{
-    // returns zero on success else non-zero
-    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
-        printf("error initializing SDL: %s\n", SDL_GetError());
-    }
-    SDL_Window* win = SDL_CreateWindow("GAME", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 600, 600, 0);
-    Uint32 render_flags = SDL_RENDERER_ACCELERATED;
-    SDL_Renderer* rend = SDL_CreateRenderer(win, -1, render_flags);
-    int close = 0;
-
-    // animation loop
-    while (!close) {
-        SDL_Event event;
-         // Events management
-        while (SDL_PollEvent(&event)) {
-            switch (event.type) {
-                case SDL_QUIT:
-                    // handling of close button
-                    close = 1;
-                    break;
-            }
-        }
-        SDL_RenderClear(rend);
-        SDL_Delay(1000 / 30);
-    }
-    SDL_DestroyRenderer(rend);
-    SDL_DestroyWindow(win);
-    SDL_Quit();
-    return 0;
-}
-
-
 int main(int argc, char *argv[])
 {
     // initialize and setup defaults for the vfs.
 	vfs_initialize();
     vfs_default();
-    //sdl_test();
 
     // load the json config.
 	std::string result = vfs_get_global()->ReadAllText("/game.json");
