c48c9506872b9bd32d7897e50fc9d1dac98ef47d
kodaloid <jakiro.fire@gmail.com>
2022-10-01 00:55:36 +0100

    Implemented fix for exception so that it doesn't force unusual compiler requirements.

--- src/Exceptions.h
+++ src/Exceptions.h
@@ -2,11 +2,27 @@
 
 #include <iostream>
 #include <exception>
+#include <string.h>
+#include <stdlib.h>
 
 using namespace std;
 
 namespace xen
 {
+	static char* strdup_ex(const char* s)
+	{
+		size_t slen = strlen(s);
+		char* result = (char *)malloc(slen + 1);
+		if(result == NULL)
+		{
+			return NULL;
+		}
+
+		memcpy(result, s, slen+1);
+		return result;
+	}
+
+
 	struct XentuNotFoundException : public exception {
 		private:
 			const char* m_file;
@@ -15,7 +31,7 @@ namespace xen
 			{
 				// use strdup to make a copy of the filename as it might get deleted
 				// before the exception is handled.
-				m_file = strdup(filename);
+				m_file = strdup_ex(filename);
 			}
 			const char * what() { return m_file; }
 	};
